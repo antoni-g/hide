e08cdfcab25adac8516a9d5fe18354f9e8ca0682
Antoni <antonig@sas.upenn.edu>
2019-01-27 00:40:27 -0500

    adjusting for auto sizing

--- index.js
+++ index.js
@@ -1,15 +1,1 @@
 console.log('running index');
-
-$( "#role" ).submit(function( event ) {
-	event.preventDefault();
-	console.log('juulious');
-	val = $('input[name="role-select"]:checked').val();
-	if (val == 'hider') {
-		console.log('rendering hider')
-		window.location.href = "hider";
-	}
-	else {
-		console.log('render seeker')
-		window.location.href = "seeker";
-	}
-});
