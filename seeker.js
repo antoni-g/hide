a398b4cf5d54f87e576691bb47b2a00583d5752d
Antoni <antonig@sas.upenn.edu>
2019-01-27 10:00:38 -0500

    fixing

--- seeker.js
+++ seeker.js
@@ -24,6 +24,7 @@ function calcDistance(lat1, lon1, lat2, lon2){  // generally used geo measuremen
     Math.sin(dLon/2) * Math.sin(dLon/2);
     var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
     var d = R * c;
+    
     return d * 1000; // meters
 }
 
