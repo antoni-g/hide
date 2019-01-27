8c3f3d407d9538a3e55f02b5a56165e603c4ab0d
Antoni <antonig@sas.upenn.edu>
2019-01-26 21:28:47 -0500

    option select

--- index.js
+++ index.js
@@ -3,4 +3,11 @@ console.log('running index');
 $( "#role" ).submit(function( event ) {
 	event.preventDefault();
 	console.log('juulious');
+	val = $('input[name="role-select"]:checked').val();
+	if (val == 'hider') {
+		console.log('rendering hider')
+	}
+	else {
+		console.log('render seeker')
+	}
 });
